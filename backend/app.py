9b594c8b3303094d05a707bcc9abfd298943ec7b
purey-878 <purey878@gmail.com>
2026-08-06 00:47:42 +0100

    new for that render to backend

--- backend/app.py
+++ backend/app.py
@@ -266,6 +266,13 @@ app.include_router(site_settings_admin_router)
 
 
 if __name__ == "__main__":
+    import os
     import uvicorn
 
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+
+    uvicorn.run(
+        "app:app",
+        host="0.0.0.0",
+        port=port,
+    )
