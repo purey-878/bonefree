61bfb09c066f5b57a966a09e9a92a28de4ce64eb
purey-878 <purey878@gmail.com>
2026-08-06 01:29:33 +0100

    asdf

--- backend/app.py
+++ backend/app.py
@@ -107,6 +107,10 @@ origins = [
     'http://localhost:5173',
     'http://127.0.0.1',
     'http://127.0.0.1:5174',
+
+    # Production frontend
+    'https://bonefree.pt',
+    'https://www.bonefree.pt',
 ]
 
 app.add_middleware(
